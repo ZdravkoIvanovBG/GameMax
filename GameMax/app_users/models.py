1b67431474790853061f8263b98dcc15e526626e
ZdravkoIvanov <114856199+ZdravkoIvanovBG@users.noreply.github.com>
2024-11-30 13:35:56 +0200

    Created AppUserManager, User and Profile Model and added Media Files Dir

---
+++ GameMax/app_users/models.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.core.validators import MinValueValidator, MinLengthValidator
+from django.db import models
+
+from GameMax.app_users.managers import AppUserManager
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+        null=True,
+        blank=True,
+        error_messages={
+            'unique': "User with this Email already exists."
+        }
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+    is_staff = models.BooleanField(
+        default=False
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = AppUserManager()
+
+
+class Profile(models.Model):
+    full_name = models.CharField(
+        max_length=100,
+        null=True,
+        blank=False,
+        validators=[
+            MinLengthValidator(2, message='Your Name Must Be At Least 2 Characters Long.')
+        ]
+    )
+
+    age = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(15),
+        ]
+    )
+
+    phone_number = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True,
+        validators=[
+            MinLengthValidator(10, message="Phone Number needs to be 10 characters long.")
+        ]
+    )
+
+    profile_picture = models.ImageField(
+        upload_to='media/profile_pictures',
+        null=True,
+        blank=True
+    )
+
+    user = models.OneToOneField(
+        to=AppUser,
+        on_delete=models.CASCADE
+    )
--- GameMax/settings.py
+++ GameMax/settings.py
@@ -16,7 +16,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'GameMax.app_users'
 ]
 
 MIDDLEWARE = [
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'GameMax.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -85,7 +84,6 @@ DATABASES = {
         'PORT': config('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -105,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -117,16 +114,17 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'app_users.AppUser'
