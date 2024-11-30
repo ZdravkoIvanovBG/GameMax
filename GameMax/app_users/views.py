a46e54c8b6600111edf76ca574b3958f729afb4c
ZdravkoIvanov <114856199+ZdravkoIvanovBG@users.noreply.github.com>
2024-11-30 13:50:50 +0200

    Created User Edit,Creation Views Logic and Forms

--- GameMax/app_users/views.py
+++ GameMax/app_users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from GameMax.app_users.forms import AppUserCreationForm
+
+UserModel = get_user_model()
+
+
+class AppUserLoginView(LoginView):
+    template_name = 'app_users/login_page.html'
+
+
+class AppUserRegisterView(CreateView):
+    model = UserModel
+    form_class = AppUserCreationForm
+    template_name = 'app_users/register_page.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        login(self.request, user=self.object)
+
+        return response
+    
