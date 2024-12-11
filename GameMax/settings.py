a0ef1a6fd48bef132410415f1ed9da7d8b834be0
ZdravkoIvanov <114856199+ZdravkoIvanovBG@users.noreply.github.com>
2024-12-11 19:17:11 +0200

    Fixed Settings

--- GameMax/settings.py
+++ GameMax/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', config('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
 
-os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
 
-os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
 
 # Application definition
 
