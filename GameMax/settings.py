706de5ea6075180541f8d3be76f0bfbae94dd199
ZdravkoIvanov <114856199+ZdravkoIvanovBG@users.noreply.github.com>
2024-12-22 18:25:57 +0200

    Changed CSRF settings

--- GameMax/settings.py
+++ GameMax/settings.py
@@ -28,7 +28,9 @@ DEBUG = os.getenv('DEBUG', config('DEBUG'))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
+# os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS'), []).split(',')
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS')).split(',')
 
 # Application definition
 
